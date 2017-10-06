21ef7dc2d81229b2c52fb6728084a16841478caf
nmchenry01 <nmchenry@live.unc.edu>
2017-10-06 14:59:52 -0400

    Added example in flask api

--- flaskAPI/app.py
+++ flaskAPI/app.py
@@ -1,7 +1,11 @@
 from flask import Flask, jsonify
 import random
+import quandl as q 
+import numpy as np
 
 from quotes import funny_quotes
+
+q.ApiConfig.api_key = "xaFxr9SP6Wd5sKFHdEax"
 
 app = Flask(__name__)
 @app.route('/', methods=['GET', 'POST'])
@@ -14,6 +18,12 @@ def serve_funny_quote():
 	selected_quote = quotes[random.randint(0, nr_of_quotes - 1)]
 	return jsonify(selected_quote)
 
+@app.route("/api/<stock>")
+def display_avg_price(stock):
+	data = q.get("EOD/{0}.4".format(stock), returns="numpy", start_date="1/1/2010", end_date="1/1/2017")
+	data_values = np.array([x[1] for x in data])
+	data = data_values.mean()
+	return jsonify({"data": data})
 
 if __name__ == "__main__":
 	app.run(debug=True)
