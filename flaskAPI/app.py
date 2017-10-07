5849fe284ce75eaf57b19ca603fa765c2b52f218
nmchenry01 <nmchenry@live.unc.edu>
2017-10-07 14:37:38 -0400

    Updated helper functions and associated API parameters

--- flaskAPI/app.py
+++ flaskAPI/app.py
@@ -22,14 +22,25 @@ def return_data(stock, start_date, end_date):
 				start_date="{0}".format(start_date), 
 				end_date="{0}".format(end_date))
 
+	#Calculate daily returns from price
+	daily_returns = data.pct_change()
+
 	#Assign current date to return
 	current_date = datetime.date.today().strftime('%m/%d/%Y')
 
 	#Assign cumulative returns
-	cumulative_returns = total_return(data)
+	cumulative_returns = total_return(daily_returns)
+
+	#Assign daily average returns
+	daily_average_return = daily_avg_return(daily_returns)
+
+	#Assign daily standard deviation 
+	daily_standard_deviation = daily_std(daily_returns)
 	
 	return jsonify({"cumulative_returns": cumulative_returns,
-					"current_date": current_date
+					"current_date": current_date,
+					"daily_average_return": daily_average_return,
+					"daily_standard_deviation": daily_standard_deviation
 })
 
 if __name__ == "__main__":
--- flaskAPI/helper_functions.py
+++ flaskAPI/helper_functions.py
@@ -6,19 +6,28 @@ import json
 #TODO
 #Add error checking to make sure the object being passed in is a pandas dataframe
 #Continue building out functions to calculate stats for flask
+#Potentially refactor to convert to percent once instead of doing it in every method
 
-#Takes in a pandas dateframe of price, outputs cumulative return
+#Takes in a pandas dateframe of returns, outputs cumulative return as float
 def total_return(x):
-    #Convert to pcts
-    pct_returns = x.pct_change()
-
     #Calculate series of cumulative returns
-    cum_returns = pct_returns.cumsum()
-
+    cum_returns = x.cumsum()
+    
     #Select the last value of the cumulative returns, convert to float 
     value = float(cum_returns.iloc[-1][0])
-    
-    return '%.3f' % round(value, 3)
+    return '%.3f' % round(value, 3) #Round to 3 decimal places
 
-def daily_avg_return(x)
+#Takes in a pandas dataframe of returns, outputs average daily return  as float
+def daily_avg_return(x):
+    avg = x.mean()
+    return '%.6f' % round(avg, 6) 
+
+#Takes in a pandas dataframe of returns, outputs daily standard deviation a
+def daily_std(x):
+    std = x.std()
+    return '%.4f' % round(std, 4) 
+
+
+
+
 
