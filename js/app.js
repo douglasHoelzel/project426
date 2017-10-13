2a1a0329b8626cc4063a0c8010f5b59ad87ee3e7
Doug <douglas.hoelzel@gmail.com>
2017-10-13 19:39:31 -0400

    changes to routing

--- js/app.js
+++ js/app.js
@@ -1,7 +1,6 @@
 (function () {
 
 var myApp = angular.module('myApp', ['ui.router','ui.bootstrap']);
-console.log("Inside of app.js");
 myApp.config(
   ["$stateProvider", "$urlRouterProvider",
     function($stateProvider, $urlRouterProvider) {
@@ -14,17 +13,13 @@ myApp.config(
           templateUrl: "home.html",
           controller: "MainController"
         })
+
         .state("assetView", {
           url: "/assetView/:tickerId",
           templateUrl: "assetView.html",
           controller: function($scope, $stateParams) {
               $scope.tickerId = $stateParams.tickerId;
           }
-        })
-        .state("landingPage", {
-          url: "/landingPage",
-          templateUrl: "landingPage.html",
-          controller: "MainController"
         })
     }
   ]);
