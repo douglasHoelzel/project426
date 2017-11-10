d8c62a503d139a6aceb9ec356ab1cae175773862
nmchenry01 <nmchenry@live.unc.edu>
2017-11-10 11:02:47 -0500

    formatting v2

--- js/app.js
+++ js/app.js
@@ -1,27 +1,27 @@
 (function () {
 
-var myApp = angular.module('myApp', ['ui.router','ui.bootstrap']);
-myApp.config(
-  ["$stateProvider", "$urlRouterProvider",
-    function($stateProvider, $urlRouterProvider) {
+  var myApp = angular.module('myApp', ['ui.router', 'ui.bootstrap']);
+  myApp.config(
+    ["$stateProvider", "$urlRouterProvider",
+      function ($stateProvider, $urlRouterProvider) {
 
-      $urlRouterProvider.otherwise("/home");
+        $urlRouterProvider.otherwise("/home");
 
-      $stateProvider
-        .state("home", {
-          url: "/home",
-          templateUrl: "home.html",
-          controller: "MainController"
-        })
+        $stateProvider
+          .state("home", {
+            url: "/home",
+            templateUrl: "home.html",
+            controller: "MainController"
+          })
 
-        .state("assetView", {
-          url: "/assetView/:tickerId",
-          templateUrl: "assetView.html",
-          controller: function($scope, $stateParams) {
+          .state("assetView", {
+            url: "/assetView/:tickerId",
+            templateUrl: "assetView.html",
+            controller: function ($scope, $stateParams) {
               $scope.tickerId = $stateParams.tickerId;
-          }
-        })
-    }
-  ]);
+            }
+          })
+      }
+    ]);
 
-  }());
+}());
