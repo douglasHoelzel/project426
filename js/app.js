c25b4ad296fb88d764e0809b0807752cdc4065d2
Doug <douglas.hoelzel@gmail.com>
2017-11-14 13:21:45 -0500

    homepage fix

--- js/app.js
+++ js/app.js
@@ -10,12 +10,12 @@
         $stateProvider
           .state("home", {
             url: "/home",
-            templateUrl: "home.html",
+            templateUrl: "assetView.html",
             controller: "MainController"
           })
 
           .state("assetView", {
-            url: "/assetView/:tickerId",
+            url: "/assetView/",
             templateUrl: "assetView.html",
             controller: function ($scope, $stateParams) {
               $scope.tickerId = $stateParams.tickerId;
@@ -25,3 +25,33 @@
     ]);
 
 }());
+
+//
+//
+// (function () {
+//
+//   var myApp = angular.module('myApp', ['ui.router', 'ui.bootstrap']);
+//   myApp.config(
+//     ["$stateProvider", "$urlRouterProvider",
+//       function ($stateProvider, $urlRouterProvider) {
+//
+//         $urlRouterProvider.otherwise("/home");
+//
+//         $stateProvider
+//           .state("home", {
+//             url: "/home",
+//             templateUrl: "home.html",
+//             controller: "MainController"
+//           })
+//
+//           .state("assetView", {
+//             url: "/assetView/:tickerId",
+//             templateUrl: "assetView.html",
+//             controller: function ($scope, $stateParams) {
+//               $scope.tickerId = $stateParams.tickerId;
+//             }
+//           })
+//       }
+//     ]);
+//
+// }());
